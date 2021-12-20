e89859fc153bd78abce17a1a3b06240434d7fceb
Dharmendra-ojha <96345964+Dharmendra-ojha@users.noreply.github.com>
2021-12-20 13:08:38 +0530

    Add files via upload

---
+++ turtle blue circle .py
@@ -0,0 +1,20 @@
+import turtle 
+t=turtle.Turtle()
+t.speed(200000000)
+
+t.shapesize(5)
+t.fillcolor("blue")
+t.pencolor("blue")
+t.pensize(10)
+
+t.begin_fill()
+t.circle(150)
+t.end_fill()
+
+t.pencolor("white")
+t.fillcolor("white")
+t .begin_fill()
+t.circle(110)
+t.end_fill()
+ 
+
---
+++ turtle geometrical design.py
@@ -0,0 +1,8 @@
+import turtle 
+t=turtle.Turtle()
+t.pencolor("blue")
+t.speed(10000000)
+for x in range(200) :
+	t.forward(x)
+	t.left(70)
+	
